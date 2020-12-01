e7f183901a7de938e3017941712007e4da32d310
nkrumahthis <nkrumahthis@gmail.com>
2020-12-01 08:28:36 +0000

    load sidebar data

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData';
+
 
 function Sidebar() {
     const {showSidebar, setShowSidebar} = useState(false);
@@ -24,6 +26,16 @@ function Sidebar() {
                             <AiIcons.AiOutlineClose/>
                         </Link>
                     </li>
+                    {SidebarData.map((item, index)=>{
+                        return(
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         </>
