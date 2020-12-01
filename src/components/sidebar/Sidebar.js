7bbad580a2f7f7df2f60116dd5817bde6a91a39d
nkrumahthis <nkrumahthis@gmail.com>
2020-12-01 08:14:18 +0000

    added close

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
 
 function Sidebar() {
+    const {showSidebar, setShowSidebar} = useState(false);
+    const toggleSidebar = (e) => {
+        e.prefentDefault();
+        setShowSidebar(!showSidebar);
+    }
+
     return (
         <>
             <div className="navbar">
                 <Link to="#" className="menu-bars">
-                    <FaIcons.FaBars/>
+                    <FaIcons.FaBars onClick={toggleSidebar}/>
                 </Link>
             </div>
+            <nav className={showSidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className="nav-menu-items">
+                    <li className="navbar-toggle">
+                        <Link className="menu-bars" to ="#">
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
